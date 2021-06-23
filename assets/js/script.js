19a960f35f4c551d9eececbb45686309e5b203ee
High_Lander <codywmarkham@gmail.com>
2021-06-22 19:35:48 -0500

    couldnt get colors to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,70 @@
+let todaysDate = document.querySelector("#todaysDate");
+const saveBtn = document.querySelectorAll(".saveBtn");
+
 let now = moment();
 
-$("#Work Day Scheduler").attr("min", now.add(1, 'd').format('YYYY-MM-DD'));
-
-const currentTimeInterval = setInterval(function () {
-  now = moment();
-  $("#now").text(`${now.format('MMM DD, YYYY')} at ${now.format('HH:mm:ss')}`);
-}, 1000);
+todaysDate = now.format("dddd, MMMM Do");
 
 
+$("#todaysDate").append(todaysDate);
+
+
+let currentHour = now.hour();
+console.log(currentHour)
 
 
 
+    const inputs = $('.text-form')
+
+    for (let i = 0; i < inputs.length; i++) {
+
+        let dataId = ($(inputs[i]).siblings("row").attr("id"));
+        console.log(dataId);
+
+        if(dataId < currentHour){
+            $("row").addClass("past");
+        } else if (dataId === currentHour){
+            $("row").addClass("present");
+        } else if (dataId > currentHour){
+            $("row").addClass("future");
+        }
+    }
+
+
+function saveText () {
+
+    if (inputs !== ""){
+        const hour = {
+            hour9: $("#hour9").val(),
+            hour10: $("#hour10").val(),
+            hour11: $("#hour11").val(),
+            hour12: $("#hour12").val(),
+            hour13: $("#hour13").val(),
+            hour14: $("#hour14").val(),
+            hour15: $("#hour15").val(),
+            hour16: $("#hour16").val(),
+            hour17: $("#hour17").val()
+        };
+
+        console.log(hour)
+
+        localStorage.setItem("hour", JSON.stringify(hour));
+    }
+}
+
+let hourEvent = JSON.parse(localStorage.getItem("hour")) || {};
+
+console.log(hourEvent)
+
+
+$("#hour9").val(hourEvent.hour9);
+$("#hour10").val(hourEvent.hour10);
+$("#hour11").val(hourEvent.hour11);
+$("#hour12").val(hourEvent.hour12);
+$("#hour13").val(hourEvent.hour13);
+$("#hour14").val(hourEvent.hour14);
+$("#hour15").val(hourEvent.hour15);
+$("#hour16").val(hourEvent.hour16);
+$("#hour17").val(hourEvent.hour17);
+
+$(".saveBtn").on("click", saveText);
